dd7c0941c4fabe49fec29cd415a6e6e9d9e5b2c4
Yuji YASUHARA <yuji.yasuhara@gmail.com>
2015-09-13 13:02:20 +0900

    improve SimpleMovement

--- Assets/SimpleMovement.cs
+++ Assets/SimpleMovement.cs
@@ -10,14 +10,28 @@ public class SimpleMovement : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		float input_v = Input.GetAxis ("Vertical");
-		float input_h = Input.GetAxis ("Horizontal");
-		if (input_v != 0) {
-			transform.rotation *= Quaternion.Euler (-input_v, 0, 0);
+		{
+			float input_v = Input.GetAxis ("Vertical");
+			float input_h = Input.GetAxis ("Horizontal");
+			const float sensitivity = 2f;
+			if (input_v != 0) {
+				transform.rotation *= Quaternion.Euler (-input_v * sensitivity, 0, 0);
+			}
+			if (input_h != 0) {
+				transform.rotation *= Quaternion.Euler (0, 0, input_h * sensitivity);
+			}
+			transform.position += transform.rotation * Vector3.forward * Time.deltaTime * 20.0f;
 		}
-		if (input_h != 0) {
-			transform.rotation *= Quaternion.Euler (0, 0, input_h);
+		{
+			float input_v = Input.GetAxis ("Mouse Y");
+			float input_h = Input.GetAxis ("Mouse X");
+			const float sensitivity = 10f;
+			if (input_v != 0) {
+				transform.rotation *= Quaternion.Euler (-input_v * sensitivity, 0, 0);
+			}
+			if (input_h != 0) {
+				transform.rotation *= Quaternion.Euler (0, input_h * sensitivity, 0);
+			}
 		}
-		transform.position += transform.rotation * Vector3.forward * Time.deltaTime * 20.0f;
 	}
 }
